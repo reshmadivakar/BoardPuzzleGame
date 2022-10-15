7c989b032a49faf7d41d3995db4392d24fba0a85
Reshma Divakar <reshmadivakar246@gmail.com>
2022-10-15 23:27:01 +0200

    [Assignment]Updated the TimeUtil class
    [Description]Implemented a better logic to convert miilis to hh:mm:ss
    using Timeutil and modulus operation

--- src/main/java/com/brs/assignment/util/TimeUtil.java
+++ src/main/java/com/brs/assignment/util/TimeUtil.java
@@ -10,14 +10,16 @@ public class TimeUtil
     /**
      * API to convert the time taken to HH MM SS readable format.
      *
-     * @param millis
+     * @param millis time in milliseconds
      *
      * @return time in HHMMSS format
+     *
+     * @link https://stackoverflow.com/a/9027379
      */
     public static String convertMillisecondsToHHMMSSFormat(long millis)
     {
         return String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
-                TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
-                TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
     }
 }
