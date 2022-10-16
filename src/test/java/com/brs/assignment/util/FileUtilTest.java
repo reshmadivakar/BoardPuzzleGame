7b9fa3590b6aef539e0373458d3f227df533a0c1
Reshma Divakar <reshmadivakar246@gmail.com>
2022-10-16 16:45:02 +0200

    [Assignment]Added unit test for File operations and time operations

--- src/test/java/com/brs/assignment/util/FileUtilTest.java
+++ src/test/java/com/brs/assignment/util/FileUtilTest.java
@@ -1,19 +1,59 @@
 package com.brs.assignment.util;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileUtilTest
 {
 
-    @Test
-    void getAllFilePathsInFolder()
+    private Path path1, path2, path3;
+    private File file1, file2, file3;
+
+    private String temFolderRootPath;
+
+    @TempDir
+    private Path tempDir;
+
+    @BeforeEach
+    public void setUp()
     {
+        try
+        {
+            path1 = tempDir.resolve("testfile1.txt");
+            path2 = tempDir.resolve("testfile2.csv");
+            path3 = tempDir.resolve("testfile3.json");
+        }
+        catch (InvalidPathException ipe)
+        {
+            System.err.println("error creating temporary test file in " + this.getClass().getSimpleName());
+        }
+
+        file1 = path1.toFile();
+        file2 = path2.toFile();
+        file3 = path3.toFile();
+
+        temFolderRootPath = file1.getParent();
+
     }
 
+
     @Test
+    @DisplayName("Test if check file ends with extension")
     void checkFileEndsWith()
     {
+        assertTrue(FileUtil.checkFileEndsWith(file1.getAbsolutePath(), ".txt"));
+        assertTrue(FileUtil.checkFileEndsWith(file2.getAbsolutePath(), ".csv"));
+        assertTrue(FileUtil.checkFileEndsWith(file3.getAbsolutePath(), ".json"));
     }
 }
---
+++ src/test/java/com/brs/assignment/util/TimeUtilTest.java
@@ -0,0 +1,25 @@
+package com.brs.assignment.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeUtilTest
+{
+
+    @Test
+    @DisplayName("Test convert milliseconds to hh:mm:ss format")
+    void convertMillisecondsToHHMMSSFormat()
+    {
+        long timeInMillis = 1000;
+        String result = TimeUtil.convertMillisecondsToHHMMSSFormat(timeInMillis);
+        assertEquals("00:00:01", result);
+        timeInMillis = 60000;
+        result = TimeUtil.convertMillisecondsToHHMMSSFormat(timeInMillis);
+        assertEquals("00:01:00", result);
+        timeInMillis = 3600000;
+        result = TimeUtil.convertMillisecondsToHHMMSSFormat(timeInMillis);
+        assertEquals("01:00:00", result);
+    }
+}
